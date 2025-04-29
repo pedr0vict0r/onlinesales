184453bc040981346744e61df7ff05a75ce739e7
Pedro Nascimento <pedro.nascimento@lepoti.tech>
2025-04-29 20:18:18 -0300

    Initial commit

---
+++ src/main/java/br/edu/xpe/onlinesales/OnlinesalesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.xpe.onlinesales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinesalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinesalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/xpe/onlinesales/OnlinesalesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.xpe.onlinesales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinesalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
